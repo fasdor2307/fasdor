f73cde67150ffb44b7dcf74e7b47768ac84b4441
fasdor2307 <50208925+fasdor2307@users.noreply.github.com>
2019-05-22 16:54:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import time
 
 while True:
 
-	vk = vk_api.VkApi(token="cf1b50f4bd5e6454d7c34c1ca8268fdd3a95ed156f443f08dc0dface12769f40c7167f9398173b05d4481")
+	vk = vk_api.VkApi(token="588a09fe546621880e553b027fa7b3f2f44ca3608728b3ab1451b3bc8015c27e789211eb08348befd9a16")
 	
 	delta = datetime.timedelta(hours=8, minutes=0) # разница от UTC. Можете вписать любое значение вместо 3
 	t = (datetime.datetime.now(datetime.timezone.utc) + delta)
